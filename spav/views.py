ea17ee665d3388bf71346ad323302458ccfe958c
Ingenious Abhinav <115482062+iamingeniousabhinav@users.noreply.github.com>
2022-10-13 17:16:45 +0530

    Another commit

--- spav/views.py
+++ spav/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+
+# call your data from models
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
