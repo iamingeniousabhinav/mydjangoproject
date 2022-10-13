6da10703b9afabb59463bc6d617886e997090cf2
Ingenious Abhinav <115482062+iamingeniousabhinav@users.noreply.github.com>
2022-10-13 21:40:49 +0530

    Added noticeboard

--- spav/models.py
+++ spav/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Noticeboard(models.Model):
+    title = models.CharField(max_length=250)
+    file = models.FileField(upload_to='Noticeboard', max_length=100)
+    upload_date = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self) -> str:
+        return self.title
--- spav/views.py
+++ spav/views.py
@@ -1,9 +1,19 @@
+from multiprocessing import context
 from django.shortcuts import render
 from .models import *
 
 # call your data from models
 
+notice_data = Noticeboard.objects.all()
+
+
+context = {
+    'notice_data' : notice_data
+}
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
+
+def noticeboard(request):
+    return render(request, 'noticeboard.html', context)
